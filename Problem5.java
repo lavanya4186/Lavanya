0f7d4c970c0e6eff392172c45487f0f66b718629
lavanya4186 <135110715+lavanya4186@users.noreply.github.com>
2023-06-02 22:45:32 +0530

    Add files via upload

---
+++ Problem5.java
@@ -0,0 +1,13 @@
+public class Problem5 {
+    public static void main(String args[])
+    {
+
+        for(int i=500;i<=1000;i++)
+        {
+            if(i==808)
+            {
+                System.out.println("The number is ==>" +i);
+            }
+        }
+    }
+}
