10b5f7219591a61686a8fab56b7f331fbe8b0ab3
lavanya4186 <135110715+lavanya4186@users.noreply.github.com>
2023-06-02 14:08:21 +0530

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,10 @@
+public class Problem1 {
+
+    public static void main(String args[])
+    {
+        float a = 8.6f;
+        float b=9.7f;
+        System.out.println("The added two float number is ==>" +(a+b));
+
+    }
+}
