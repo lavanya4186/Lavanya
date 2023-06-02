f376611d8caa0e99c3de52b950acd070b2e09588
lavanya4186 <135110715+lavanya4186@users.noreply.github.com>
2023-06-03 00:00:21 +0530

    Add files via upload

---
+++ Problem6.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Problem6 {
+    public static void main(String args[]) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for (int i = 200; i <= 500; i++) {
+             a.add(i);
+            System.out.println("The a[" +i+ "] is added in arraylist" );
+        }
+    }
+}
